23e869a97783bb359b824709502fc4b143c634aa
lorenzogralav <lorenzogra2022@libero.it>
2024-09-17 14:15:50 +0200

    Primo commit

---
+++ Whistleblowing.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Whistleblowing.NET.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
