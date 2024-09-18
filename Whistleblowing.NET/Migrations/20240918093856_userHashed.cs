45255afc90bfae86387bf856e88b8a0750d4eba5
giuliacimino <giuliacimino00@gmail.com>
2024-09-18 11:46:21 +0200

    modificato campo e aggiunto userHashed

---
+++ Whistleblowing.NET/Migrations/20240918093856_userHashed.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Whistleblowing.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class userHashed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "user_id",
+                table: "SegnalazioneAnonymous",
+                newName: "user_hashed");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "user_hashed",
+                table: "SegnalazioneAnonymous",
+                newName: "user_id");
+        }
+    }
+}
