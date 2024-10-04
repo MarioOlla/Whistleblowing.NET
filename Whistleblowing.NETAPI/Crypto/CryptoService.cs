7df22b0ebfdc254b9448778f602919010b201a6d
Cristian <piarullicristian97@gmail.com>
2024-10-04 12:26:28 +0200

    Update CryptoService e Program.cs

--- Whistleblowing.NETAPI/Crypto/CryptoService.cs
+++ Whistleblowing.NETAPI/Crypto/CryptoService.cs
@@ -1,4 +1,6 @@
-﻿using Whistleblowing.NETAPI.Data;
+﻿using System.Security.Cryptography;
+using System.Text;
+using Whistleblowing.NETAPI.Data;
 using Whistleblowing.NETAPI.Models;
 
 namespace Whistleblowing.NETAPI.Crypto
@@ -18,6 +20,170 @@ namespace Whistleblowing.NETAPI.Crypto
         {
             _context.CryptoKey.Add(info);
             _context.SaveChanges();
+        }
+
+        //Viene richiamato nel momento in cui il db non ha valori di tipo CryptoKey.
+        public static CryptoKey GenerateCryptoData(string password, int aesIterations, int aesKeySize)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                // Esportazione della chiave pubblica in formato Base64
+                string publicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
+
+                // Generazione del salt
+                byte[] salt = GenerateSalt();
+
+                // Esportazione della chiave privata e cifratura con la password dell'amministratore
+                byte[] privateKeyBytes = rsa.ExportRSAPrivateKey();
+                Console.WriteLine("Lunghezza private key: {0}", privateKeyBytes.Length);
+                string encryptedPrivateKey = EncryptPrivateKeyWithPassword(privateKeyBytes, password, salt, aesIterations, aesKeySize);
+
+                //Console.WriteLine("\n encrypted rsa private key: {0}\n\n", encryptedPrivateKey);
+                //Console.WriteLine("rsa public key: {0}\n\n", publicKey);
+                //Console.WriteLine("salt lenght e saltB64: {0} - {1}\n\n", salt.Length, Convert.ToBase64String(salt));
+                //Console.WriteLine("aes key size: {0}\n\n", aesKeySize);
+                //Console.WriteLine("aes iterations: {0}\n\n", aesIterations);
+
+                // Salva i dati crittografici nel database
+                return new CryptoKey
+                {
+                    EncryptedRsaPrivateKey = encryptedPrivateKey,
+                    RsaPublicKey = publicKey,
+                    Salt = Convert.ToBase64String(salt),
+                    AesKeySize = aesKeySize,  // Dimensione chiave AES in bit
+                    AesIterations = aesIterations  // Numero di iterazioni PBKDF2
+                };
+            }
+        }
+
+        private static byte[] GenerateSalt()
+        {
+            byte[] salt = new byte[16];  // 128 bit di salt
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);  // Riempie il salt con byte casuali
+            }
+            return salt;
+        }
+
+        public static RSAParameters LoadPublicKey(string publicKey)
+        {
+            if (publicKey == null)
+                throw new Exception("Chiave pubblica non trovata!");
+
+            // Importa la chiave pubblica nel formato RSAParameters
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportRSAPublicKey(Convert.FromBase64String(publicKey), out _);
+                return rsa.ExportParameters(false);
+            }
+        }
+
+        public static RSAParameters LoadPrivateKey(string adminPassword, string privateKeyEncrypted, string saltString,int aesIterations, int aesKeySize)
+        {
+            if (privateKeyEncrypted == null)
+                throw new Exception("Chiave privata non trovata!");
+            if (saltString == null)
+                throw new Exception("Salt non trovato!");
+
+            byte[] salt = Convert.FromBase64String(saltString);
+
+            // Decifrare la chiave privata usando la password dell'amministratore
+            byte[] privateKeyBytes = DecryptPrivateKeyWithPassword(privateKeyEncrypted, adminPassword, salt, aesIterations, aesKeySize);
+            Console.WriteLine(privateKeyBytes.Length);
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportRSAPrivateKey(privateKeyBytes, out _);
+                return rsa.ExportParameters(true);
+            }
+        }
+
+        private static string EncryptPrivateKeyWithPassword(byte[] privateKey, string password, byte[] salt, int iterations, int aesKeySize)
+        {
+            if (salt.Length == 0)
+                throw new Exception("Salt non trovato!");
+
+            Console.WriteLine($"Lunghezza chiave privata prima della cifratura: {privateKey.Length}");
+
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, salt, iterations, HashAlgorithmName.SHA256))
+            {
+                byte[] key = deriveBytes.GetBytes(aesKeySize / 8);  // Ottieni la chiave AES in byte
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = key;
+                    aes.GenerateIV();  // Generazione dell'IV
+
+                    byte[] encryptedKey;
+
+                    using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                    using (var ms = new MemoryStream())
+                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        cs.Write(privateKey, 0, privateKey.Length);
+                        cs.FlushFinalBlock();
+                        encryptedKey = ms.ToArray();
+                    }
+
+                    Console.WriteLine($"IV - chiave privata cifrata: {Convert.ToBase64String(aes.IV)} - lunghezza: {encryptedKey.Length}");
+                    return $"{Convert.ToBase64String(aes.IV)}:{Convert.ToBase64String(encryptedKey)}";  // Ritorna IV e chiave cifrata
+                }
+            }
+        }
+
+        private static byte[] DecryptPrivateKeyWithPassword(string encryptedPrivateKey, string password, byte[] salt, int aesIterations, int aesKeysize)
+        {
+            var parts = encryptedPrivateKey.Split(':');
+            byte[] iv = Convert.FromBase64String(parts[0]);  // Estrai IV
+            byte[] encryptedKeyBytes = Convert.FromBase64String(parts[1]);  // Estrai chiave cifrata
+
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, salt, aesIterations, HashAlgorithmName.SHA256))
+            {
+                byte[] key = deriveBytes.GetBytes(aesKeysize / 8);
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = key;
+                    aes.IV = iv;
+
+                    using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                    using (var ms = new MemoryStream(encryptedKeyBytes))
+                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    using (var resultStream = new MemoryStream())
+                    {
+                        cs.CopyTo(resultStream); // Copia tutto il contenuto dal CryptoStream al MemoryStream
+                        return resultStream.ToArray(); // Ritorna la chiave privata decifrata
+                    }
+                }
+            }
+        }
+
+        public static byte[] EncryptWithRSA(RSAParameters publicKey, string data)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportParameters(publicKey);
+                return rsa.Encrypt(Encoding.UTF8.GetBytes(data), RSAEncryptionPadding.OaepSHA256);
+            }
+        }
+
+        public static string DecryptWithRSA(RSAParameters privateKey, byte[] encryptedData)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportParameters(privateKey);
+                byte[] decryptedBytes = rsa.Decrypt(encryptedData, RSAEncryptionPadding.OaepSHA256);
+                return Encoding.UTF8.GetString(decryptedBytes);
+            }
+        }
+
+        public static void PrintCryptoDataToConsole(CryptoKey data)
+        {
+            Console.WriteLine("\n encrypted rsa private key: {0}\n\n", data.EncryptedRsaPrivateKey);
+            Console.WriteLine("rsa public key: {0}\n\n", data.RsaPublicKey);
+            Console.WriteLine("saltB64: {0}\n\n", data.Salt);
+            Console.WriteLine("aes key size: {0}\n\n", data.AesKeySize);
+            Console.WriteLine("aes iterations: {0}\n\n", data.AesIterations);
         }
     }
 }
--- Whistleblowing.NETAPI/Program.cs
+++ Whistleblowing.NETAPI/Program.cs
@@ -2,83 +2,76 @@ using Whistleblowing.NETAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Text.Json.Serialization;
-using Microsoft.OpenApi.Any;
-using Microsoft.OpenApi.Models;
 using Whistleblowing.NETAPI.Models;
-using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Identity;
+using Whistleblowing.NETAPI.Crypto;
+using System.Security.Cryptography;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-	builder.Services.AddDbContext<WhistleBlowingContext>(options =>
-		options.UseSqlServer(builder.Configuration.GetConnectionString("WhistleBlowingContext") ?? throw new InvalidOperationException("Connection string 'WhistleBlowingContext' not found.")));
-	builder.Services.AddIdentity<User, IdentityRole>(options =>
-	{
-		options.Password.RequireDigit = true;
-		options.Password.RequireLowercase = true;
-		options.Password.RequireUppercase = true;
-		options.Password.RequireNonAlphanumeric = false;
-		options.Password.RequiredLength = 6;
+builder.Services.AddDbContext<WhistleBlowingContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WhistleBlowingContext") ?? throw new InvalidOperationException("Connection string 'WhistleBlowingContext' not found.")));
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
 
 
-	})
-		.AddEntityFrameworkStores<WhistleBlowingContext>()
-		.AddDefaultTokenProviders();
+})
+    .AddEntityFrameworkStores<WhistleBlowingContext>()
+    .AddDefaultTokenProviders();
 
 
 
 // Aggiungi il contesto del database
 builder.Services.AddDbContext<WhistleBlowingContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("WhistleBlowingContext")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WhistleBlowingContext")));
 
 // servizio web e applicazione che effettua richieste hanno domini differenti
 // quindi, abilitando CORS, consento ad altri domini di chiamare l'API
 builder.Services.AddCors(options =>
-	options.AddDefaultPolicy(policy =>
-		policy.WithOrigins("http://localhost:44300")
-		.AllowAnyOrigin()
-		.AllowAnyMethod()
-		.AllowAnyHeader()
-		));
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:44300")
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        ));
 builder.Services.AddCors(options =>
-	options.AddDefaultPolicy(policy =>
-		policy.WithOrigins("http://localhost:44316")
-		.AllowAnyOrigin()
-		.AllowAnyMethod()
-		.AllowAnyHeader()
-		));
-
-
-
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:44316")
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        ));
 
 builder.Services.AddControllers().AddJsonOptions(x =>
-	x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-	   .AddCookie(options =>
-	   {
+       .AddCookie(options =>
+       {
 
-		   options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-		   options.Cookie.Name = "Cookie"; // Specifica il nome del cookie
-										   //options.Cookie.HttpOnly = true;
-										   //options.Cookie.SecurePolicy = CookieSecurePolicy.None; // Imposta la politica di sicurezza del cookie
-										   //options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.None; // Imposta SameSite su Strict per proteggere da attacchi CSRF
-		   options.Cookie.IsEssential = true; // Imposta il cookie come essenziale per le richieste
-		   options.AccessDeniedPath = "/Unauthorized/ErrorPage"; // Imposta il percorso di accesso negato per il reindirizzamento
-		   options.LoginPath = "/Auth/Login";
-	   });
+           options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+           options.Cookie.Name = "Cookie"; // Specifica il nome del cookie
+                                           //options.Cookie.HttpOnly = true;
+                                           //options.Cookie.SecurePolicy = CookieSecurePolicy.None; // Imposta la politica di sicurezza del cookie
+                                           //options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.None; // Imposta SameSite su Strict per proteggere da attacchi CSRF
+           options.Cookie.IsEssential = true; // Imposta il cookie come essenziale per le richieste
+           options.AccessDeniedPath = "/Unauthorized/ErrorPage"; // Imposta il percorso di accesso negato per il reindirizzamento
+           options.LoginPath = "/Auth/Login";
+       });
 
 //
 // Add services to the container.
 builder.Services.AddControllers()
-	.AddJsonOptions(options =>
-	{
-		options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-	});
-
-
-
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
 
 // Add services to the container.
@@ -88,19 +81,62 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-	c.CustomSchemaIds(type => type.FullName);
+    c.CustomSchemaIds(type => type.FullName);
 
-}); ;
+});
+
+builder.Services.AddTransient<CryptoService>();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    // Risolvi il servizio scoped (CryptoService)
+    var cryptoService = scope.ServiceProvider.GetRequiredService<CryptoService>();
+
+    try
+    {
+
+        CryptoKey cryptoData = cryptoService.fetchCryptoInfo();
+
+        string passwordAdmin = "lallero";
+
+        if (cryptoData == null)
+        {
+            cryptoData = CryptoService.GenerateCryptoData(passwordAdmin, 10000, 256);
+            cryptoService.saveCryptoInfo(cryptoData);
+            Console.WriteLine("INFO ******> Ho creato una nuova configurazione per la crittografia");
+        }
+
+        CryptoService.PrintCryptoDataToConsole(cryptoData);
+
+        string msg = "Mio messaggio non cifrato";
+
+        Console.WriteLine("INFO ******> Inizio encrypt dei dati");
+        byte[] msgC = CryptoService.EncryptWithRSA(CryptoService.LoadPublicKey(cryptoData.RsaPublicKey), msg);
+        Console.WriteLine("INFO ******> Fine encrypt dei dati");
+        //stampa
+
+        RSAParameters pk = CryptoService.LoadPrivateKey(passwordAdmin, cryptoData.EncryptedRsaPrivateKey, cryptoData.Salt, cryptoData.AesIterations,cryptoData.AesKeySize);
+
+        string msgD = CryptoService.DecryptWithRSA(pk, msgC);
+
+        Console.WriteLine("Messaggio decrittato: " + msgD);
+    }
+    catch (Exception ex)
+    {
+        // Logga o gestisci l'errore se qualcosa va storto
+        Console.WriteLine("Errore durante il setup della crittografia: {0} \n\n {1}",ex.Message, ex.StackTrace);
+    }
 }
 
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
