4b1d456407319e99665c93ac8eb14c5b0495229d
Cristian <piarullicristian97@gmail.com>
2024-11-04 14:45:14 +0100

    Aggiunte modifiche

---
+++ Whistleblowing.NET/Models/DTO/SegnalazioneRegularDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Whistleblowing.NET.Models.DTO
+{
+    public class SegnalazioneRegularDTO
+    {
+
+        public int Id { get; set; }
+        public DateTime DataEvento { get; set; }
+        public string? SoggettoColpevole { get; set; }
+
+    }
+}
---
+++ Whistleblowing.NETAPI/Models/PaginatedSegnalazioniRegularViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Whistleblowing.NETAPI.Models;
+
+namespace Whistleblowing.NET.Models
+{
+    public class PaginatedSegnalazioniRegularViewModel
+    {
+
+        public int Id_segnalazioneRegular { get; set; }
+        public DateTime DataEvento { get; set; }
+        public string? SoggettoColpevole { get; set; } 
+        public List<SegnalazioneRegular> SegnalazioniRegulars { get; set; } // Questo rappresenta la lista delle segnalazioni
+
+        // Paginazione
+        public List<int> Number { get; set; } = new List<int> { 5, 10, 15 }; // Opzioni per la selezione del numero di elementi per pagina
+        public int NumberSelected { get; set; } = 5; // Numero di elementi selezionati per pagina
+        public int PageNumber { get; set; } = 1; // Numero della pagina corrente
+        public int PageSize { get; set; } = 10; // Dimensione della pagina
+        public int TotalItems { get; set; } // Numero totale di segnalazioni
+        public string SortBy { get; set; } = "Id_segnalazione"; // Campo per ordinamento
+        public bool SortDesc { get; set; } = true; // Ordinamento decrescente o crescente
+        // Fine paginazione
+    }
+}
